a6d866a893541409830e128fa314445fecc07c44
Corey Leigh Latislaw <colabug@gmail.com>
2017-06-25 09:58:19 -0700

    Fixed a test compilation error

--- app/src/test/java/com/codepath/apps/songbirder/login/LoginActivityTest.java
+++ app/src/test/java/com/codepath/apps/songbirder/login/LoginActivityTest.java
@@ -1,7 +1,6 @@
 package com.codepath.apps.songbirder.login;
 
 import android.content.Intent;
-import android.view.View;
 import android.widget.Button;
 
 import com.codepath.apps.songbirder.BuildConfig;
@@ -93,7 +92,7 @@ public class LoginActivityTest
         }
 
         @Override
-        public void loginToRest( View view )
+        public void loginToRest()
         {
             // Don't call Twitter API, record locally instead
             loggedIn = true;
