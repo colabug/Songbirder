3cfacfd6a4f172d43929adcd1c12d85362f44728
Corey Leigh Latislaw <colabug@gmail.com>
2017-06-25 11:54:24 -0700

    Revert "Updated how dialog was constructed, added a behavior"
    This reverts commit bf5be12d45ac5ed30103cdbe9d61b8445c6453de.

--- app/src/main/java/com/codepath/apps/songbirder/ComposeTweetBottomSheet.java
+++ app/src/main/java/com/codepath/apps/songbirder/ComposeTweetBottomSheet.java
@@ -1,54 +1,17 @@
 package com.codepath.apps.songbirder;
 
-import android.app.Dialog;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialogFragment;
-import android.support.design.widget.CoordinatorLayout;
+import android.view.LayoutInflater;
 import android.view.View;
-
+import android.view.ViewGroup;
 
 public class ComposeTweetBottomSheet extends BottomSheetDialogFragment
 {
-    private BottomSheetBehavior.BottomSheetCallback behaviorCallback = getBottomSheetCallback();
-
     @Override
-    public void setupDialog( Dialog dialog, int style )
+    public View onCreateView( LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState )
     {
-        super.setupDialog( dialog, style );
-        View contentView = View.inflate( getContext(), R.layout.fragment_bottom_sheet, null );
-        dialog.setContentView( contentView );
-        CoordinatorLayout.Behavior behavior = getLayoutParams( contentView ).getBehavior();
-        if( behavior != null && behavior instanceof BottomSheetBehavior )
-        {
-            ( (BottomSheetBehavior) behavior ).setBottomSheetCallback( behaviorCallback );
-        }
-    }
-
-    private CoordinatorLayout.LayoutParams getLayoutParams( View contentView )
-    {
-        return (CoordinatorLayout.LayoutParams) ( (View) contentView.getParent() ).getLayoutParams();
-    }
-
-    @NonNull
-    private BottomSheetBehavior.BottomSheetCallback getBottomSheetCallback()
-    {
-        return new BottomSheetBehavior.BottomSheetCallback()
-        {
-            @Override
-            public void onStateChanged( @NonNull View bottomSheet, int newState )
-            {
-                if( newState == BottomSheetBehavior.STATE_HIDDEN )
-                {
-                    dismiss();
-                }
-            }
-
-            @Override
-            public void onSlide( @NonNull View bottomSheet, float slideOffset )
-            {
-            }
-        };
+        return inflater.inflate( R.layout.fragment_bottom_sheet, container );
     }
 }
