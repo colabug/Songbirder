e75b5aa9dbffe08ea56874e48c922658e740d17a
Corey Leigh Latislaw <colabug@gmail.com>
2017-07-04 20:34:19 -0700

    Fleshed out User parsing, centralized key naming

--- app/src/main/java/com/codepath/apps/songbirder/models/User.java
+++ app/src/main/java/com/codepath/apps/songbirder/models/User.java
@@ -9,28 +9,55 @@ import org.parceler.Parcel;
 @Parcel
 public class User
 {
-    private static final String NAME_KEY = "name";
-    private static final String ID_KEY = "id";
-    private static final String SCREEN_NAME_KEY = "screen_name";
-    private static final String PROFILE_IMAGE_URL_KEY = "profile_image_url";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_ID = "id";
+    private static final String KEY_SCREEN_NAME = "screen_name";
+    private static final String KEY_PROFILE_IMAGE_URL = "profile_image_url";
+    private static final String KEY_BACKGROUND_IMAGE_URL = "profile_background_image_url";
+    private static final String KEY_LOCATION = "location";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_URL = "url";
+    private static final String KEY_FOLLOWER_COUNT = "followers_count";
+    private static final String KEY_FRIEND_COUNT = "friends_count";
+    private static final String KEY_VERIFIED = "verified";
 
     long uid;
     String name;
     String screenName;
+    private boolean verified;
+
     String profileImageUrl;
+    String backgroundImageUrl;
+
+    String location;
+    String description;
+    String url;
+
+    int followerCount;
+    int friendCount;
 
     protected User()
     {
     }
 
-    static User fromJson( JSONObject jsonObject ) throws JSONException
+    public static User fromJson( JSONObject jsonObject ) throws JSONException
     {
         User user = new User();
 
-        user.name = jsonObject.getString( NAME_KEY );
-        user.uid = jsonObject.getLong( ID_KEY );
-        user.screenName = jsonObject.getString( SCREEN_NAME_KEY );
-        user.profileImageUrl = jsonObject.getString( PROFILE_IMAGE_URL_KEY );
+        user.name = jsonObject.getString( KEY_NAME );
+        user.uid = jsonObject.getLong( KEY_ID );
+        user.screenName = jsonObject.getString( KEY_SCREEN_NAME );
+        user.verified = jsonObject.getBoolean( KEY_VERIFIED );
+
+        user.profileImageUrl = jsonObject.getString( KEY_PROFILE_IMAGE_URL );
+        user.backgroundImageUrl = jsonObject.getString( KEY_BACKGROUND_IMAGE_URL );
+
+        user.location = jsonObject.getString( KEY_LOCATION );
+        user.description = jsonObject.getString( KEY_DESCRIPTION );
+        user.url = jsonObject.getString( KEY_URL );
+
+        user.followerCount = jsonObject.getInt( KEY_FOLLOWER_COUNT );
+        user.friendCount = jsonObject.getInt( KEY_FRIEND_COUNT );
 
         return user;
     }
